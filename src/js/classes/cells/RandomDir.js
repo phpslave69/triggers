0b245be8216f33706b7ba53292fb194cdb0cdb8a
vinitskyss <sergey.h12@gmail.com>
2018-03-20 14:25:36 +0700

    prepare to pull

--- src/js/classes/cells/RandomDir.js
+++ src/js/classes/cells/RandomDir.js
@@ -1,30 +1,30 @@
-class RandomDir extends ChangeDir{
-	constructor(x, y, lockDir, changeRate){
-		super(x, y, lockDir);
-		this.textColor = 'black';
-		this.color = 'cyan';
-		this.lockDir = lockDir;
-		this.dirs = [MOVEUP, MOVEDOWN, MOVELEFT, MOVERIGHT];
-		this.rate = changeRate;
-		this.getDir();
-		this.setText();
-	}
+class RandomDir extends ChangeDir {
+    constructor(x, y, lockDir, changeRate) {
+        super(x, y, lockDir);
+        this.textColor = 'black';
+        this.color = 'cyan';
+        this.lockDir = lockDir;
+        this.dirs = [MOVEUP, MOVEDOWN, MOVELEFT, MOVERIGHT];
+        this.rate = changeRate;
+        this.getDir();
+        this.setText();
+    }
 
-	getDir(){
-		if(tick % this.rate == 0 && ticked){
-			while(true){
-				this.dir = this.dirs[Math.floor(random(0, 4))];		
-				if(this.lockDir.includes(this.dir) == false){
-					this.setText();
-					break;
-				}
-			}	
-		}
-	}
+    getDir() {
+        if (tick % this.rate == 0 && ticked) {
+            while (true) {
+                this.dir = this.dirs[Math.floor(random(0, 4))];
+                if (this.lockDir.includes(this.dir) == false) {
+                    this.setText();
+                    break;
+                }
+            }
+        }
+    }
 
-	update(){
-		this.getDir();
-		this.changeDir();
-		this.show();
-	}
+    update() {
+        this.getDir();
+        this.changeDir();
+        this.show();
+    }
 }
