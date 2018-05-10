fe82cd024354b8943146858b664e0a89b9029f32
Vinitskyss <sergey.h12@gmail.com>
2018-05-10 14:00:23 +0700

    Added reverser (unnfinished)

--- src/js/classes/cells/Reverser.js
+++ src/js/classes/cells/Reverser.js
@@ -1,5 +1,23 @@
 class Reverser extends Cell{
   constructor(x, y){
     super(x, y);
+    this.color = 'blue';
+    this.textColor = 'white';
   }
+
+  getDir(dir){
+    if(dir == UP){
+      return DOWN;
+    }
+    if(dir == DOWN){
+      return UP;
+    }
+    if(dir == LEFT){
+      return RIGHT;
+    }
+    if(dir == RIGHT){
+      return LEFT;
+    }
+  }
+
 }
